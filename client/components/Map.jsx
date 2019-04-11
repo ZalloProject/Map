35d36d5b4b99e516b09df5b98f1aea05914f6f87
Hack Reactor Student <student@hackreactor.com>
2019-04-10 20:12:05 -0700

    finished most of client styling, starting working on main page render

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -2,39 +2,15 @@ import React from 'react';
 import style from './styles.css';
 import { compose, withProps } from 'recompose';
 import { GoogleMap, withGoogleMap, withScriptjs, Circle } from 'react-google-maps';
-import MarkerWithLabel from 'react-google-maps/lib/components/addons/MarkerWithLabel';
-const ZalloMap = ({ houses, lat, lng, handleClick, handleMouseOver }) => {
+import Marker from './Marker.jsx';
+const ZalloMap = ({ houses, lat, lng, hoverChange }) => {
   let key = 0;
   return (
     <GoogleMap defaultZoom={10} defaultCenter={{ lat, lng }}>
       {houses
         ? houses.map(house => {
             return house.lat !== undefined ? (
-              <MarkerWithLabel
-                onClick={handleClick}
-                key={key++}
-                position={{ lat: house.lat, lng: house.lng }}
-                labelAnchor={new google.maps.Point(17, -0.1)}
-                // labelStyle={{
-                //   backgroundColor: '#faf7f4',
-                //   color: '#5f5f5f',
-                //   fontSize: '12px',
-                //   fontFamily: '"Open Sans",Gotham,gotham,Tahoma,Geneva,sans-serif',
-                //   boxShadow: '0 1px #ccc',
-                //   margin: '5px'
-                // }}
-                // labelClass={style.label}
-                icon={{
-                  // url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Red_Circle%28R%3D204%2CGB%3D0%29.svg/2000px-Red_Circle%28R%3D204%2CGB%3D0%29.svg.png',
-                  url: '',
-                  scaledSize: new google.maps.Size(10, 10)
-                }}
-              >
-                <div className={style.labelContainer}>
-                  <div className={style.circle} />
-                  <div>${Math.round(house.price / 1000)}k</div>
-                </div>
-              </MarkerWithLabel>
+              <Marker key={key++} hoverChange={hoverChange} house={house} />
             ) : null;
           })
         : null}
---
+++ client/components/Marker.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import style from './styles.css';
+import MarkerWithLabel from 'react-google-maps/lib/components/addons/MarkerWithLabel';
+import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox';
+import InfoWindow from 'react-google-maps/lib/components/InfoWindow';
+class CustomMarker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false
+    };
+  }
+  render() {
+    return (
+      <div>
+        <MarkerWithLabel
+          position={{ lat: this.props.house.lat, lng: this.props.house.lng }}
+          labelAnchor={new google.maps.Point(17, -0.1)}
+          onMouseOver={e => {
+            !document.getElementById('infobox') && this.setState({ hovered: true });
+          }}
+          onMouseOut={e => {
+            this.setState({ hovered: false });
+          }}
+          onClick={e => console.log(this.props.house)}
+          icon={{
+            url: '',
+            scaledSize: new google.maps.Size(10, 10)
+          }}
+        >
+          <div className={style.label}>
+            <div className={!this.state.hovered ? style.circle : style.circleHover} />
+            <div className={!this.state.hovered ? style.labelPrice : style.labelPriceHover}>
+              ${Math.round(this.props.house.price / 1000)}k
+            </div>
+          </div>
+        </MarkerWithLabel>
+        {this.state.hovered && !document.getElementById('infobox') && (
+          <InfoWindow
+            position={new google.maps.LatLng(this.props.house.lat, this.props.house.lng)}
+            options={{
+              enableEventPropagation: true,
+              disableAutoPan: true,
+              closeBoxURL: ''
+            }}
+            onCloseClick={e => this.setState({ hovered: false })}
+          >
+            <div id="infobox" className={style.infoBoxContainer}>
+              <img src={this.props.house.pictureURL} style={{ width: '35px', height: '35px' }} />
+              <div className={style.textContainer}>
+                <div className={style.price}>${this.props.house.price / 1000}k</div>
+                <div className={style.moreInfo}>
+                  {this.props.house.beds} bd, {this.props.house.baths} ba
+                </div>
+              </div>
+            </div>
+          </InfoWindow>
+        )}
+      </div>
+    );
+  }
+}
+export default CustomMarker;
